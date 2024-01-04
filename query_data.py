5aeb5e59617ab0b550e94da4848623c713159836
Farid Bellameche <faridautomatic@gmail.com>
2024-01-04 14:48:06 -0500

    add data to farid-RAG.txt and force by the prompt reply in french

--- query_data.py
+++ query_data.py
@@ -16,10 +16,11 @@ Entrée de suivi: {question}
 Question autonome:"""
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
-template = """Vous êtes un assistant aux questions sur la présentation de Farid Bellameche à Confoo.
+template = """Vous êtes un assistant aux questions sur la présentation de Farid Bellameche à Confoo. Vous ne répondez qu'en FRANCAIS SVP!!!
 On vous donne les parties extraites suivantes d'un long document et une question. Fournissez une réponse conversationnelle.
 Si vous ne connaissez pas la réponse, dites simplement "Hmm, je ne répond qu'aux question concernant Confoo et Farid". N'essayez pas d'inventer une réponse.
 Si la question ne porte pas sur la présentation de Farid Bellameche à Confoo, informez-les poliment que vous ne répondez qu'aux questions concernant Confoo et Farid.
+
 Question: {question}
 =========
 {context}
@@ -34,7 +35,7 @@ def get_chain(vectorstore):
 
     manager = CallbackManager([MyCallbackHandler()])
 
-    llm = OpenAI(temperature=0, callback_manager=manager)
+    llm = OpenAI(temperature=0, callback_manager=manager, model_name="gpt-3.5-turbo")
     qa_chain = ChatVectorDBChain.from_llm(
         llm,
         vectorstore,
